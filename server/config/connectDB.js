b30c875a3df6a4ce9d6456e0978bec63e5d94cdb
rohan209547mourya <rohan.209547@gmail.com>
2023-03-25 15:07:45 +0530

    last backend test

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -3,7 +3,8 @@ require('dotenv').config()
 
 module.exports = async function() {
     try {
-        await mongoose.connect(`mongodb+srv://rohanmourya671:${process.env.DB_PASSWORD}@taskplannerclustor.3mryuzl.mongodb.net/?retryWrites=true&w=majority/task-planner-db`)
+        await mongoose.connect(`mongodb+srv://rohanmourya671:${process.env.DB_PASSWORD}@taskplannerclustor.3mryuzl.mongodb.net/task-planner-db?retryWrites=true&w=majority`)
+        // mongodb+srv://rohanmourya671:${process.env.DB_PASSWORD}@taskplannerclustor.3mryuzl.mongodb.net/?retryWrites=true&w=majority
 
         console.log("Connected to MongoDB");
     } 
